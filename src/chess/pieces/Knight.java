c32808fa2802abe7bcebea8aff8dd579598b664d
Renato de Souza <renbstux@gmail.com>
2022-01-19 11:04:04 -0300

    Queen

--- src/chess/pieces/Knight.java
+++ src/chess/pieces/Knight.java
@@ -52,17 +52,17 @@ public class Knight extends ChessPiece {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		p.setValues(position.getRow() + 2, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		p.setValues(position.getRow() + 2, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 
-		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		p.setValues(position.getRow() + 1, position.getColumn() - 2);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
